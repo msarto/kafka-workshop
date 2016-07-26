c29828891f5e4c193a00e69e01ed39690eef6a31
Ovidiu Petridean <opetridean@sdl.com>
2016-07-26 19:19:05 +0300

    added the twitter stuff

---
+++ src/main/java/org/bigdata/workshop/TwiterKafkaBridge.java
@@ -0,0 +1,33 @@
+package org.bigdata.workshop;
+
+import java.util.StringJoiner;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * Created by opetridean on 7/26/16.
+ */
+public class TwiterKafkaBridge {
+
+    private CustomKafkaProducer customKafkaProducer;
+    private BlockingQueue<String> tweetsQueue;
+
+
+    public TwiterKafkaBridge(CustomKafkaProducer customKafkaProducer, BlockingQueue<String> tweetsQueue) {
+        this.customKafkaProducer = customKafkaProducer;
+        this.tweetsQueue = tweetsQueue;
+    }
+
+    /**
+     * Produce for the topic brexit
+     * @throws InterruptedException
+     */
+    public void producerForBrexit() throws InterruptedException {
+        for (int msgRead = 0; msgRead < 1000; msgRead++) {
+            String msg = tweetsQueue.take();
+            customKafkaProducer.send("brexit", "", msg);
+            System.out.println(msg);
+        }
+    }
+
+
+}
